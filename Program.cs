ab31702845b49f9400a1fd9a4339de1bd54a6a5f
Romeo Calota <mail@romeocalota.me>
2023-03-07 12:38:31 +0200

    Add first code content

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿class C
+{
+    public string Name { get; set; }
+} 
+
+struct S {}
+
+
+class Base
+{
+}
+
+interface IInterface
+{
+}
+
+interface IInterface2
+{
+}
+
+abstract class AbstractBase
+{
+}
+
+class Derived : Base, IInterface, IInterface2
+{
+}
+
+record R(string Name, int Age);
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        C c1 = new C()
+        {
+            Name = "John"
+        };
+        
+        C c2 = new C()
+        {
+            Name = "John"
+        };
+        
+        R r1 = new R("John", 20);
+        R r2 = new R("John", 20);
+        
+        Console.WriteLine(r1 == r2);
+        Console.WriteLine(c1 == c2);
+        Console.WriteLine(r1);
+        Console.WriteLine(c1);
+    }
+}
