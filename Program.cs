79c265f2a3bf09a11d61de8b3e5497b66b7c5695
Romeo Calota <mail@romeocalota.me>
2023-03-15 07:46:47 +0200

    Add code from second lab

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Google.Protobuf;
+
+{
+    Node n1 = new Node();
+    n1.Id = 1;
+    n1.Lat = 2;
+    n1.Lon = 3;
+
+    using var stream = new FileStream("node.bin", FileMode.Create);
+    n1.WriteTo(stream);
+    stream.Flush();
+}
+
+var node2 = Node.Parser.ParseFrom(File.ReadAllBytes("node.bin"));
+Console.WriteLine(node2);
